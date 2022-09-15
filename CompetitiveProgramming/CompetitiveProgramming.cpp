ba6f907a11d36b78e2ebdc6a3c56ec302ef49e21
Phazerous <NPhazerous@gmail.com>
2022-09-16 00:57:19 +0300

    Task 4 v.2
    Перебор разбиений заданного числа на слагаемые.

--- CompetitiveProgramming/CompetitiveProgramming.cpp
+++ CompetitiveProgramming/CompetitiveProgramming.cpp
@@ -6,61 +6,48 @@ using namespace std;
 ifstream cin("input.txt");
 ofstream cout("output.txt");
 
+int n;
 vector<int> buffer;
 
-int n;
-
-void rec();
-void out();
-int calcSum();
+void rec(int idx, int sum, int last);
+void show(int idx);
 
 int main() {
 	cin >> n;
 
-	rec();
+	buffer = vector<int>(n);
 
+	rec(0, 0, 0);
+	
 	return 0;
 }
 
-int calcSum() {
-	int sum = 0;
-
-	for (int num : buffer) {
-		sum += num;
-	}
-
-	return sum;
-}
-
-void rec() {
-	int currSum = calcSum();
-
-	if (currSum == n) {
-		out();
+void rec(int idx, int sum, int last) {
+	if (sum == n) {
+		show(idx);
 		return;
 	}
 
-	for (int i = 1; i <= n - currSum; i++) {
-		if (currSum == 0 || (i >= buffer[buffer.size() - 1] && currSum + i <= n)) {
-			buffer.push_back(i);
-			rec();
-			buffer.pop_back();
-		}
+	for (int i = last; i <= n - sum; i++) {
+		if (i == 0)
+			continue;
+
+		buffer[idx] = i;
+		rec(idx + 1, sum + i, i);
 	}
 }
 
-void out() {
-	for (int i = 0; i < buffer.size(); i++) {
-		if (i == 0) {
-			cout << n << " = ";
-		}
+void show(int idx) {
+	cout << n << " = ";
 
+	for (int i = 0; i < idx; i++) {
 		cout << buffer[i];
 
-		if (i != buffer.size() - 1) {
-			cout << " + ";
-		}
+		if (i == idx - 1)
+			break;
+
+		cout << " + ";
 	}
 
-	cout << '\n';
+	cout << "\n";
 }
